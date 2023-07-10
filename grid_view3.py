96b41434f59a57f57b296b1729c1db54a11511d1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-07-10 15:57:23 +0200

    Update grid_view3.py

--- grid_view3.py
+++ grid_view3.py
@@ -24,12 +24,9 @@ def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    x = 0.0
-    z = 0.0
-    persp = 0.1
 
-    glClearColor(0.0, 0.0, 0.0, 1.0)#(0.0, 0.0, 1.0, 1.0)  # Establece el color de fondo en azul (RGB: 0.0, 0.0, 1.0)
-    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0) #45
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
     glTranslatef(0.0, 0.0, -7.0)
     glRotatef(7, 1, 0, 0)
     
@@ -42,41 +39,30 @@ def main():
 
         # CONTROL DE DIRECCIÓN
         if key[pygame.K_LEFT]:
-            x = 0.050
-            z = 0.0
+            glTranslatef(0.050, 0.0, 0.0)
         if key[pygame.K_RIGHT]:
-            x = -0.050
-            z = 0.0
+            glTranslatef(-0.050, 0.0, 0.0)
         if key[pygame.K_UP]:
-            z = 0.050
-            x = 0.0
+            glTranslatef(0.0, 0.0, 0.050)
         if key[pygame.K_DOWN]:
-            z = -0.050
-            x = 0.0
+            glTranslatef(0.0, 0.0, -0.050)
         if key[pygame.K_o]:
-            x = 0.050
-            z = 0.050
+            glTranslatef(0.050, 0.0, 0.050)
         if key[pygame.K_p]:
-            x = -0.050
-            z = 0.050
+            glTranslatef(-0.050, 0.0, 0.050)
         if key[pygame.K_k]:
-            x = -0.050
-            z = -0.050
+            glTranslatef(-0.050, 0.0, -0.050)
         if key[pygame.K_l]:
-            x = 0.050
-            z = -0.050
-        if key[pygame.K_s]:
-            x = 0.0
-            z = 0.0
+            glTranslatef(0.050, 0.0, -0.050)
+
         # ROTACIONES
         if key[pygame.K_t]:
-            glRotatef(1, 0, 1, 0)
+            glRotatef(0.050, 0, 1, 0)
         if key[pygame.K_r]:
-            glRotatef(1, 1, 0, 0)
+            glRotatef(0.050, 1, 0, 0)
         if key[pygame.K_y]:
-            glRotatef(1, 0, 0, 1)
+            glRotatef(0.050, 0, 0, 1)
             
-        glTranslatef(x, 0.0, z)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         draw_grid()
         pygame.display.flip()
