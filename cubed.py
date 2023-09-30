af2f6ae0cd416c0496cde1e8fcfc35b120c5f001
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-09-30 15:01:22 +0200

    Create cubed.py

---
+++ cubed.py
@@ -0,0 +1,105 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+verticies = [
+    [1, 0, -1],
+    [1, 1, -1],
+    [-1, 1, -1],
+    [-1, 0, -1],
+    [1, 0, 1],
+    [1, 1, 1],
+    [-1, 0, 1],
+    [-1, 1, 1]
+    ]
+
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+    )
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+    )
+
+# DIBUJA CONTORNOS DEL CUBO
+def Cube():
+    glLineWidth(2.0)
+    glBegin(GL_LINES)
+    glColor3f(1.0,0.0,0.0,)
+    for edge in edges:  
+        x=0
+        for vertex in edge:
+            x+=1
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+# DIBUJA CUBO SOBRE EL GRID    
+def CubeB():
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE)
+    glCullFace(GL_FRONT)
+    glBegin(GL_QUADS)
+    glColor3f(0.0,0.0,0.1)
+    for surface in surfaces: 
+        x=0
+        for vertex in surface:
+            x+=1
+            #glColor3fv(colors[x])
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+# FUNCIÓN PRINCIPAL
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    rot = False
+    
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -7.0)
+    
+    running = True
+    while (running):
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                running = False
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_p]:
+            rot = True
+            
+        # ROTACIONES
+        if key[pygame.K_UP]:
+            glRotatef(0.1, -1, 0, 0)       
+        if key[pygame.K_DOWN]:
+            glRotatef(0.1, 1, 0, 0)
+            
+        if rot == True:
+            glRotatef(1, 0, 1, 0)
+        glTranslatef(0.0, 0.0, 0.0)  
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        CubeB()
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+    pygame.quit()
+         
+main()
