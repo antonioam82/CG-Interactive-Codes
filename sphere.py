74dc200c44625721d360bdfffb363a0ac3c9ad0c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-03-30 16:06:06 +0100

    Update sphere.py

--- sphere.py
+++ sphere.py
@@ -4,14 +4,14 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 '''def draw_sphere():
-    glColor3f(1.0, 1.0, 1.0)  # Color blanco
+    glColor3f(1.0, 0.0, 1.0)  # Color blanco
     gluSphere(gluNewQuadric(), 1, 32, 32)  # Crea una esfera con radio 1'''
 
 def draw_sphere():
     glColor3f(1.0, 1.0, 1.0)  # Color blanco
     quad = gluNewQuadric()
     gluQuadricDrawStyle(quad, GLU_LINE)  # Establecer el estilo de dibujo a líneas
-    gluSphere(quad, 1, 32, 32)  # Crea una esfera con radio 1
+    gluSphere(quad, 1, 32, 32)  # Crea una esfera con radio 1'''
 
 def main():
     pygame.init()
@@ -22,6 +22,8 @@ def main():
     glRotatef(90, 1, 0, 0)
     glRotatef(-23, 0, 1, 0)
     
+    scale_factor = 1.0 ######################
+    
 
     clock = pygame.time.Clock()
     
@@ -30,12 +32,25 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        #___________________________________
+        key = pygame.key.get_pressed()
+        
+        if key[pygame.K_UP]:
+            scale_factor += 0.1
+
+        elif  key[pygame.K_DOWN]:
+            scale_factor -= 0.1
+        #____________________________________
         
         #glRotatef(1, 3, 1, 1)  # Rotación de la esfera
         glRotatef(1, 0, 0, 1)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glPushMatrix()#####3######3
+        glScalef(scale_factor, scale_factor, scale_factor)
         draw_sphere()
+        glPopMatrix()###################3
         pygame.display.flip()
         clock.tick(30)
 
