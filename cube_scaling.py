bedb37aa6f9b67d0d4f53be21d35cf72dc161ecd
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-03-30 16:35:36 +0100

    Update cube_scaling.py

--- cube_scaling.py
+++ cube_scaling.py
@@ -29,8 +29,30 @@ edges = (
     (5, 7)
 )
 
-def Cube():
+grid_size = 120
+grid_spacing = 1
+
+# DIBUJA GRID
+def draw_grid():
+    glLineWidth(1.0)
     glBegin(GL_LINES)
+    glColor3f(0.0,1.0,0.0)#(0.5, 0.5, 0.5)  # Color gris
+    
+    for x in range(-grid_size, grid_size + 1, grid_spacing):
+        glVertex3f(x, 0, -grid_size)
+        glVertex3f(x, 0, grid_size)
+
+    #glColor3f(1.0,0.0,0.0)
+    for z in range(-grid_size, grid_size + 1, grid_spacing):
+        glVertex3f(-grid_size, 0, z)
+        glVertex3f(grid_size, 0, z)
+
+    glEnd()
+
+def Cube():
+    glLineWidth(3.0)
+    glBegin(GL_LINES)
+    glColor3f(0.0, 0.0, 1.0)  # Color azul para el cubo
     for edge in edges:
         for vertex in edge:
             glVertex3fv(vertices[vertex])
@@ -67,6 +89,11 @@ def main():
         
         glRotatef(1, 0, 1, 0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glPushMatrix()
+        glTranslatef(0.0, 0.0, -20.0)
+        glRotatef(20, 1, 0, 0)
+        draw_grid()
+        glPopMatrix()
         glPushMatrix()
         glScalef(scale_factor, scale_factor, scale_factor)
         Cube()
