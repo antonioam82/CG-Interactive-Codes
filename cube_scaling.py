9148fb011b4786aa1bad0620a7209ba0d1f6ed6a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-03-30 15:42:24 +0100

    Update cube_scaling.py

--- cube_scaling.py
+++ cube_scaling.py
@@ -65,7 +65,7 @@ def main():
             scale_factor -= 0.1
             
         
-
+        glRotatef(1, 0, 1, 0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glPushMatrix()
         glScalef(scale_factor, scale_factor, scale_factor)
