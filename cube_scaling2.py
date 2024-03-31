743d0a1a14022ba49833dbc2ab34ec35a57c7c5d
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-03-31 12:09:06 +0200

    Create cube_scaling2.py

---
+++ cube_scaling2.py
@@ -0,0 +1,93 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
+
+def Cube():
+    glLineWidth(2.0)
+    glBegin(GL_LINES)
+    glColor3f(0.0, 0.0, 1.0)  # Color azul para el cubo
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -10)
+
+    scale_factor = 1
+    scale_factor2 = 1
+
+    angle = 0
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        key = pygame.key.get_pressed()
+        
+        if key[pygame.K_UP]:
+            scale_factor += 0.1
+            scale_factor2 -= 0.1
+            
+
+        elif  key[pygame.K_DOWN]:
+            scale_factor -= 0.1
+            scale_factor2 += 0.1
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        
+        # Cubo 1
+        glPushMatrix()
+        glTranslatef(-2, 0, 0)
+        glScalef(scale_factor, scale_factor, scale_factor)
+        glRotatef(angle, 1, 1, 0)
+        Cube()
+        glPopMatrix()
+        
+        # Cubo 2
+        glPushMatrix()
+        glTranslatef(2, 0, 0)
+        glScalef(scale_factor2, scale_factor2, scale_factor2)
+        glRotatef(angle, -1, 1, 0)
+        Cube()
+        glPopMatrix()
+
+        angle += 1
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+    pygame.quit()
+
+main()
