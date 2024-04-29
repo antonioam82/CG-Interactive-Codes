5b3c06e556e34eb796001207cca8bd04b11b250f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-04-29 15:58:42 +0200

    Create pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,123 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+
+grid_size = 120
+grid_spacing = 1
+
+vertices = [
+    [1, 0.15, -1],  # 0
+    [1, 0.15, 1],   # 1
+    [-1, 0.15, 1],  # 2
+    [-1, 0.15, -1], # 3
+    [0, 1.15, 0]    # 4 (vértice superior de la pirámide)
+]
+
+edges = (
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 0),
+    (0, 4),
+    (1, 4),
+    (2, 4),
+    (3, 4)
+)
+
+surfaces = (
+    (0, 4, 3),
+    (1, 4, 0),
+    (2, 4, 1),
+    (3, 4, 2),
+    (0, 3, 2, 1),  # Base triangular
+)
+
+
+def Pyramid():
+    glBegin(GL_QUADS)
+    glColor3f(1.0,0.0,0.0)
+    for surface in surfaces: 
+        x=0
+        for vertex in surface:
+            x+=1
+            glVertex3fv(vertices[vertex])
+    glEnd()
+    
+    glLineWidth(2.0)
+    glBegin(GL_LINES)
+    glColor3f(1.0,1.0,0.0,)
+    for edge in edges:  
+        x=0
+        for vertex in edge:
+            x+=1
+            glVertex3fv(vertices[vertex])
+    glEnd()
+    
+def draw_grid():
+    glBegin(GL_LINES)
+    glColor3f(1.0,1.0,1.0)
+
+    for x in range(-grid_size, grid_size + 1, grid_spacing):
+        glVertex3f(x, 0, -grid_size)
+        glVertex3f(x, 0, grid_size)
+
+    for z in range(-grid_size, grid_size + 1, grid_spacing):
+        glVertex3f(-grid_size, 0, z)
+        glVertex3f(grid_size, 0, z)
+
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, -1.5, -8.5)
+    glRotatef(7, 1, 0, 0)
+    angle = 0
+    
+    running = True
+    while (running):
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                running = False
+
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_g]:
+            glRotatef(-0.1, 1, 0, 0)
+        elif key[pygame.K_r]:
+            glRotatef(0.1, 1, 0, 0)
+        elif key[pygame.K_LEFT]:
+            glTranslatef(0.1, 0, 0)
+        elif key[pygame.K_RIGHT]:
+            glTranslatef(-0.1, 0, 0)
+        elif key[pygame.K_UP]:
+            glTranslatef(0, 0, 0.1)
+        elif key[pygame.K_DOWN]:
+            glTranslate(0, 0, -0.1)
+            
+    
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Grid
+        glPushMatrix()
+        draw_grid()
+        glPopMatrix()
+
+        # Pirámide
+        glPushMatrix()
+        glRotatef(angle, 0, 1, 0)
+        Pyramid()
+        glPopMatrix()
+
+        angle += 2
+        pygame.display.flip()
+        pygame.time.wait(10)
+    pygame.quit()
+         
+main()
