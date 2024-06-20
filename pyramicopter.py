4c7477457e1aa16b5af839b7a030b6d15bbbe3e0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-06-20 19:09:04 +0200

    Update pyramicopter.py

--- pyramicopter.py
+++ pyramicopter.py
@@ -40,9 +40,9 @@ def Pyramid():
     glBegin(GL_QUADS)
     glColor3f(1.0,0.0,0.0)
     for surface in surfaces: 
-        x=0
+        #x=0
         for vertex in surface:
-            x+=1
+            #x+=1
             glVertex3fv(vertices[vertex])
     glEnd()
     
