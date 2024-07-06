f5d017c5e7acf747ea8b5c1a089b481f198becdb
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-07-06 17:47:25 +0200

    Update import_obj.py

--- import_obj.py
+++ import_obj.py
@@ -32,3 +32,57 @@ modelFaces = get_list(facestxt)
 # Para depuración, imprime los primeros elementos de las listas
 print("Model Vertices:", modelVerts[:5])
 print("Model Faces:", modelFaces[:5])
+
+def drawfaces():
+    glClear(GL_COLOR_BUFFER_BIT or GL_DEPHT_BUFFER_BIT)
+    glBegin(GL_TRIANGLES)
+    for eachface in (modelFaces):
+        color = 2
+        for eachvert in eachface:
+            color += 1
+            if color >5:
+                color = 0
+            glColor3fv(paints[color])
+            glVertex3fv(modelVerts[int(eachvert)])
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800,800)
+    pygame.display.set_caption("RENDERING OBJECT")
+    FPS = pygame.time.Clock()
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    gluPerspective(45,1,.1,50)
+    glTranslate(0,-1,-5)
+    glRotate(0,1,0,0)
+
+    Left = False
+    Right = False
+
+    def moveOBJ():
+        if Left:
+            glRotate(-1,0,1,0)
+        if Right:
+             glRotate(1,0,1,0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_a:
+                    Left = True
+                if event.key == K_d:
+                    Right = True
+            if event.type == KEYUP:
+                if event.key == K_a:
+                    Left = False
+                if event.key == K_d:
+                    Right = False
+
+        pygame.display.flip()
+        drawfaces()
+        moveOBJ()
+        FPS.tick(60)
+main()
