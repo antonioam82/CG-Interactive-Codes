434317c4864065afed9f0497eb7bd978b1c8e66e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-07-08 10:11:15 +0200

    Create quaternions_example.py

---
+++ quaternions_example.py
@@ -0,0 +1,136 @@
+import glfw
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np
+import math
+
+class Quaternion:
+    def __init__(self, w, x, y, z):
+        self.w = w
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def to_matrix(self):
+        ww, xx, yy, zz = self.w * self.w, self.x * self.x, self.y * self.y, self.z * self.z
+        wx, wy, wz = self.w * self.x, self.w * self.y, self.w * self.z
+        xy, xz, yz = self.x * self.y, self.x * self.z, self.y * self.z
+
+        return np.array([
+            [1 - 2 * (yy + zz), 2 * (xy - wz), 2 * (xz + wy), 0],
+            [2 * (xy + wz), 1 - 2 * (xx + zz), 2 * (yz - wx), 0],
+            [2 * (xz - wy), 2 * (yz + wx), 1 - 2 * (xx + yy), 0],
+            [0, 0, 0, 1]
+        ], dtype=np.float32)
+
+    def __mul__(self, other):
+        w1, x1, y1, z1 = self.w, self.x, self.y, self.z
+        w2, x2, y2, z2 = other.w, other.x, other.y, other.z
+        return Quaternion(
+            w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2,
+            w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2,
+            w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2,
+            w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+        )
+
+def create_rotation_quaternion(angle, x, y, z):
+    half_angle = math.radians(angle) / 2.0
+    sin_half_angle = math.sin(half_angle)
+    return Quaternion(math.cos(half_angle), x * sin_half_angle, y * sin_half_angle, z * sin_half_angle)
+
+def main():
+    if not glfw.init():
+        return
+
+    window = glfw.create_window(800, 600, "Cuaterniones en OpenGL", None, None)
+    if not window:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(window)
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(45, 800 / 600, 0.1, 50.0)
+    glMatrixMode(GL_MODELVIEW)
+
+    quaternion = Quaternion(1, 0, 0, 0)
+
+    def key_callback(window, key, scancode, action, mods):
+        nonlocal quaternion
+        if action == glfw.PRESS or action == glfw.REPEAT:
+            if key == glfw.KEY_UP:
+                rotation = create_rotation_quaternion(5, 1, 0, 0)
+                quaternion = quaternion * rotation
+            elif key == glfw.KEY_DOWN:
+                rotation = create_rotation_quaternion(-5, 1, 0, 0)
+                quaternion = quaternion * rotation
+            elif key == glfw.KEY_LEFT:
+                rotation = create_rotation_quaternion(5, 0, 1, 0)
+                quaternion = quaternion * rotation
+            elif key == glfw.KEY_RIGHT:
+                rotation = create_rotation_quaternion(-5, 0, 1, 0)
+                quaternion = quaternion * rotation
+
+    glfw.set_key_callback(window, key_callback)
+
+    while not glfw.window_should_close(window):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0)
+
+        rotation_matrix = quaternion.to_matrix()
+        glMultMatrixf(rotation_matrix)
+
+        glBegin(GL_QUADS)
+        
+        # Front face (red)
+        glColor3f(1, 0, 0)
+        glVertex3f(-1, -1, 1)
+        glVertex3f(1, -1, 1)
+        glVertex3f(1, 1, 1)
+        glVertex3f(-1, 1, 1)
+
+        # Back face (green)
+        glColor3f(0, 1, 0)
+        glVertex3f(-1, -1, -1)
+        glVertex3f(-1, 1, -1)
+        glVertex3f(1, 1, -1)
+        glVertex3f(1, -1, -1)
+
+        # Left face (blue)
+        glColor3f(0, 0, 1)
+        glVertex3f(-1, -1, -1)
+        glVertex3f(-1, -1, 1)
+        glVertex3f(-1, 1, 1)
+        glVertex3f(-1, 1, -1)
+
+        # Right face (yellow)
+        glColor3f(1, 1, 0)
+        glVertex3f(1, -1, -1)
+        glVertex3f(1, 1, -1)
+        glVertex3f(1, 1, 1)
+        glVertex3f(1, -1, 1)
+
+        # Top face (magenta)
+        glColor3f(1, 0, 1)
+        glVertex3f(-1, 1, -1)
+        glVertex3f(-1, 1, 1)
+        glVertex3f(1, 1, 1)
+        glVertex3f(1, 1, -1)
+
+        # Bottom face (cyan)
+        glColor3f(0, 1, 1)
+        glVertex3f(-1, -1, -1)
+        glVertex3f(1, -1, -1)
+        glVertex3f(1, -1, 1)
+        glVertex3f(-1, -1, 1)
+
+        glEnd()
+
+        glfw.swap_buffers(window)
+        glfw.poll_events()
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
