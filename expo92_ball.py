febf05bc2ba28767b0f69126adb4f72f626dc74e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-07-15 16:24:32 +0200

    Update expo92_ball.py

--- expo92_ball.py
+++ expo92_ball.py
@@ -9,7 +9,8 @@ def draw_sphere():
     glColor3f(1.0, 1.0, 0.0)
     quad = gluNewQuadric()
     gluQuadricDrawStyle(quad, GLU_LINE)  # Establecer el estilo de dibujo a líneas
-    gluSphere(quad, 1, 32, 32)  # Crea una esfera con radio 1
+    gluSphere(quad, 1, 32, 32)
+    #gluSphere(quad, 1.003, 32, 32)
 
 def main():
     pygame.init()
@@ -18,6 +19,10 @@ def main():
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
     glTranslatef(0.0, 0.0, -5)  # Mueve la esfera hacia atrás para que sea visible
 
+    '''glEnable(GL_DEPTH_TEST)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)'''
+    
     clock = pygame.time.Clock()
     running = True
     while running:
