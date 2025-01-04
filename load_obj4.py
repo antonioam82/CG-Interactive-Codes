b67e1a0ecdb1f505f1fb01083c1dc7f5d9826ee1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2025-01-04 16:37:54 +0100

    Update load_obj4.py

--- load_obj4.py
+++ load_obj4.py
@@ -22,11 +22,19 @@ def check_height_value(height):
         raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+f"Height value must be less than 900 and greater than 600."+Fore.RESET+Style.RESET_ALL)
     return val
 
+def check_source_ext(file):
+    name, ex = os.path.splitext(file)
+    if os.path.exists(file):
+        if ex != ".obj":
+            raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+f"Source file must be '.obj' ('{ex}' is not valid)."+Fore.RESET+Style.RESET_ALL)
+    else:
+        raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+f"FILE NOT FOUND: file or path '{file}' not found."+Fore.RESET+Style.RESET_ALL)
+    return file
 
 def main():
     parser = argparse.ArgumentParser(prog="ModelVisor0.1", conflict_handler='resolve',
                                      description="Show obj models")
-    parser.add_argument('-load','--load_object',required=False,help="Obj model to load")
+    parser.add_argument('-load','--load_object',required=True,type=check_source_ext,help="Obj model to load")
     parser.add_argument('-width','--window_width',type=check_width_value,default=800,help="Widow width")
     parser.add_argument('-height','--window_height',type=check_height_value,default=600,help="Window height")
 
