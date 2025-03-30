af7dfc21c6a75bb8248d9eac002a57bf7c128034
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2025-03-30 19:35:38 +0200

    Update terrain.py

--- terrain.py
+++ terrain.py
@@ -135,11 +135,14 @@ def main():
     # Bucle principal
     running = True
     clock = pygame.time.Clock()
-
+    
     while running:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
             '''elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     orientation = -90
@@ -252,8 +255,7 @@ def main():
         pygame.display.flip()
         clock.tick(60)
 
-    pygame.quit()
+    pygame.quit()          
 
 if __name__ == "__main__":
     main()
-
