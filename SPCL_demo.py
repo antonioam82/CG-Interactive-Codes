a20f7ec666f85e48bc2c18c265ddae0bcbc99849
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2025-04-16 17:14:37 +0200

    Update SPCL_demo.py

--- SPCL_demo.py
+++ SPCL_demo.py
@@ -10,6 +10,62 @@ from OpenGL.GLU import *
 grid_size = 140
 grid_spacing = 1
 
+vertices = (
+    (1.0, 0.0, -1.0),
+    (1.0, 0.2, -1.0),
+    (-1.0, 0.2, -1.0),
+    (-1.0, 0.0, -1.0),
+    (1.0, 0.0, 1.0),
+    (1.0, 0.2, 1.0),
+    (-1.0, 0.0, 1.0),
+    (-1.0, 0.2, 1.0)
+)
+ 
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
+ 
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+    )
+
+def draw_plattform():
+    platt_list = glGenLists(1)
+    glNewList(platt_list, GL_COMPILE)
+    glLineWidth(3.0)
+    glBegin(GL_LINES)
+    glColor3f(0.2,0.4,0.3)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+    glBegin(GL_QUADS)
+    glColor3f(0.0,0.0,0.0)
+    for surface in surfaces:
+        for vertex in surface:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+    glEndList()
+    return platt_list
+
 
 def draw_grid():
     grid_list = glGenLists(1)
@@ -36,10 +92,12 @@ def main():
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     gluPerspective(45, (display[0] / display[1]), 0.1, 90.0)
     glTranslatef(0.0,0.0,-10)
+    glEnable(GL_DEPTH_TEST)
     glRotatef(15,1,0,0)
     glClearColor(0.3, 0.3, 0.3, 1.0)
 
     grid_list = draw_grid()
+    platt_list = draw_plattform()
 
     running = True
 
@@ -54,10 +112,15 @@ def main():
             glRotatef(0.4,0,1,0)
         elif key_button[pygame.K_t]:
             glRotatef(-0.4,0,1,0)
+        elif key_button[pygame.K_y]:
+            glRotatef(0.4,1,0,0)
+        elif key_button[pygame.K_u]:
+            glRotatef(-0.4,1,0,0)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         glCallList(grid_list)
+        glCallList(platt_list)
 
         pygame.display.flip()
         pygame.time.wait(10)
