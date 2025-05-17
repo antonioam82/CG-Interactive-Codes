50f0f4ac633e007909d4863fd6cde791b87d7aa8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2025-05-17 17:13:43 +0200

    Create spinning_quat.py

---
+++ spinning_quat.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+import numpy as np
+
+
+verticies = [
+    [0, 1, 0],#top
+    [1, 0, -1],#del der
+    [-1, 0, -1],#del iz
+    [1, 0, 1],#tras der
+    [-1, 0, 1],#tras iz
+    [0, -1, 0]#bottom
+    ]
+    
+edges =(
+    (0,1),
+    (0,2),
+    (0,3),
+    (0,4),
+    (1,3),
+    (2,1),
+    (4,2),
+    (4,3),
+    (5,1),
+    (5,2),
+    (5,3),
+    (5,4)
+    )
+
+surfaces = (
+    (3,0,4),
+    (4,0,2),
+    (2,0,1),
+    (1,0,3),
+    (3,5,4),
+    (4,5,2),
+    (2,5,1),
+    (1,5,3)
+    )
+
+
+colors = (
+    (1,1,0),
+    (0,1,0),
+    (1,0,1),
+    (0,1,1),
+    (0,1,1),
+    (0,1,0),
+    )
+
+def draw_figure():
+    glBegin(GL_TRIANGLES)
+    for surface in surfaces:
+        x=0
+        for vertex in surface:
+            x+=1
+            glColor3fv(colors[x])
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(35, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0,0.0,-5)
+    glRotatef(90,1,0,0)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+        draw_figure()
+
+    pygame.quit()
+
+if __name__ =="__main__":
+    main()
