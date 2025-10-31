b521b0d57e1e7f4a607b870ebee7392beafd1889
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2025-10-31 09:58:40 +0100

    Update grid_view_demo.py

--- grid_view_demo.py
+++ grid_view_demo.py
@@ -130,8 +130,8 @@ def main():
     glEnable(GL_MULTISAMPLE)
     glEnable(GL_LINE_SMOOTH)
     glHint(GL_LINE_SMOOTH_HINT, GL_NICEST)
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    #glEnable(GL_BLEND)
+    #glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
     
     gluPerspective(45, (display[0] / display[1]), 0.1, 90.0) #90
     glTranslatef(0.0, 0.0, -10)
