06aa1c1eeb0db069289b4230a05d0f48c6d82c84
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2025-11-14 11:36:47 +0100

    Update load_obj4b4.py

--- load_obj4b4.py
+++ load_obj4b4.py
@@ -125,8 +125,8 @@ def show_controls():
     print("  - 'P' Key: Toggle between Orthographic and Perspective views")
 
     print("\nZoom Controls:")
-    print("  - 'Z' Key: Zoom in (decrease scale)")
-    print("  - 'X' Key: Zoom out (increase scale)")
+    print("  - 'Z' Key: Zoom out (decrease scale)")
+    print("  - 'X' Key: Zoom in (increase scale)")
     print("  - Mouse Wheel: Zoom in/out")
 
     print("\nTranslation & Rotation Controls (Drag):")
