a99b417da489ae93fa021f5f4c7749cf52c64f4a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2026-03-12 15:44:31 +0100

    Refactor load_obj function and argument handling

--- script_prueba.py
+++ script_prueba.py
@@ -1,119 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- argparse
+from load_obj4c5 import load_obj
 import argparse
 
 
-
-def load_obj(args):
-    vertices: list[list[float]] = []
-    faces: list[list[int]] = []
-    edges: set[tuple[int, int]] = set()
-    num_verts: int = 0
-    num_triangles: int = 0
-    num_edges: int = 0
-    polygon_verts: int = 0
-    load_error: bool = False
-    print(args.load_object)
-
-    try:
-        with open(args.load_object, 'r') as file:
-
-            for line in file:
-               #print(line)
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-
-                parts = line.split()
-
-                # VERTICES
-                if parts[0] == 'v':
-                    if len(parts) < 4:
-                        continue
-
-                    vertex = [
-                        float(parts[1]),
-                        float(parts[2]),
-                        float(parts[3])
-                    ]
-                    vertices.append(vertex)
-                    num_verts += 1
-
-                # FACES
-                elif parts[0] == 'f':
-
-                    face_indices: list[int] = []
-                    for part in parts[1:]:
-
-                        # soporta v, v/vt, v//vn, v/vt/vn
-                        vals = part.split('/')
-                        if vals[0] == '':
-                            continue
-                        idx = int(vals[0])
-
-                        # soporte índices negativos OBJ
-                        if idx < 0:
-                            idx = len(vertices) + idx
-                        else:
-                            idx -= 1
-                        face_indices.append(idx)
-
-                    if len(face_indices) < 4:
-                        continue
-
-                    polygon_verts = len(face_indices)
-
-                    if args.color:
-                        faces.append(face_indices)
-
-                    num_triangles += 1
-
-                    # generar edges
-                    for i in range(len(face_indices)):
-                        v1 = face_indices[i]
-                        v2 = face_indices[(i + 1) % len(face_indices)]
-                        edges.add(tuple(sorted((v1, v2))))
-
-        num_edges = len(edges)
-        print(num_edges)
-
-        # CENTERING
-        '''if args.enable_centering and vertices:
-            verts_np = np.array(vertices)
-            min_v = np.min(verts_np, axis=0)
-            max_v = np.max(verts_np, axis=0)
-            center = (min_v + max_v) / 2.0
-
-            vertices = [list(np.array(v) - center) for v in vertices]'''
-
-    except Exception as e:
-        print(f'ERROR: {str(e)}')
-        load_error = True
-    print('\n-----------------------------')
-    print(f'NV: {num_verts}')
-    print(f'NF: {num_triangles}')
-    print('-----------------------------\n')
-    #print("OK!")
-    item_ = args.show_item
-    if item_ == 'vertices':
-        print('VERTICES:')
-        print(vertices)
-    elif item_ == 'edges':
-        print('EDGES:')
-        print(edges)
-    elif item_ == 'faces':
-        print('FACES:')
-        print(faces)
-    #return vertices, edges, num_verts, num_triangles, num_edges, faces, polygon_verts, load_error
+def check_item(item):
+    items = ['faces','edges','vertices']
+    if item not in items:
+        raise argparse.ArgumentTypeError("Item must be 'faces', 'edges' or 'vertices'.")
+    return item
 
 
 def main():
     parser = argparse.ArgumentParser(prog="script_prueba", conflict_handler='resolve',
                                      description="Check obj reading",allow_abbrev=False)
     parser.add_argument('-load','--load_object',required=True,type=str,help="Obj model to load")
-    parser.add_argument('-item','--show_item',required=True,type=str,help="Info to show")
+    parser.add_argument('-item','--show_item',required=True,type=check_item,help="Info to show")
     parser.add_argument('-clr','--color',action='store_true',help='Use color')
+    parser.add_argument('--enable_centering', action='store_true', help='Center model')
 
     args = parser.parse_args()
-    load_obj(args)
+    try:
+        filename = args.load_object
+        color = args.color
+        v, e, nv, nt, ne, f, pv, le = load_obj(filename,color,args)
+        item_ = args.show_item
+        long = 30-(len(filename))
+        print(f"\n{filename}{'-'*long}")
+        print(f"NUM VERTS: {nv}")
+        print(f"NUM FACES: {nt}")
+        print(f"NUM EDGES: {ne}")
+        print(f"POL VERTS: {pv}")
+        print(f"LDL ERROR: {le}")
+        print('-'*30)
+        if item_ == 'vertices':
+            print('VERTICES:')
+            print(v)
+        elif item_ == 'edges':
+            print('EDGES:')
+            print(e)
+        elif item_ == 'faces':
+            print('FACES:')
+            print(f)
+    except Exception as e:
+        print(f"ERROR: {str(e)}.")
 
 if __name__ =="__main__":
     main()
